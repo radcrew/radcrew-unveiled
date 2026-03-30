e0b73f1f1f5a1243a398a320997a71b0c780ca0d
realactioner <realactioner@gmail.com>
2026-03-29 19:41:19 -0700

    feat: create contentful loader api

---
+++ backend/app/knowledge/contentful_loader/constants.py
@@ -0,0 +1,7 @@
+"""Contentful CDN and content-type identifiers."""
+
+from __future__ import annotations
+
+CDN_BASE = "https://cdn.contentful.com"
+ENGINEERS_CONTENT_TYPE = "engineers"
+USER_AGENT = "radcrew-unveiled-kb-loader"
