b5768467aa78cee801fd3531e34164a04991b1b5
engdotme <engdotme@proton.me>
2026-06-13 00:55:19 -0700

    test: add frontend unit and component tests

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,5 +1,8 @@
 import "@testing-library/jest-dom";
 
+// jsdom does not implement scrollIntoView; components that auto-scroll need it.
+Element.prototype.scrollIntoView = () => {};
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: (query: string) => ({
